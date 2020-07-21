097756e679cee699fa460b7175e0a9a7f93635ff
Henrique Ferreira <ihenriqueferr@gmail.com>
2020-07-21 03:53:50 -0300

    Respondendo a 6

--- src/exercicio06/Exercicio06.java
+++ src/exercicio06/Exercicio06.java
@@ -3,8 +3,8 @@ package exercicio06;
 public class Exercicio06 {
 
 	public static void main(String[] args) {
-		System.out.println("Encapsulamento: ");
-		System.out.println("Polimorfismo: ");
+		System.out.println("Encapsulamento: é o ato de dividir a aplicação em partes, deixando-as em formas isoladas.");
+		System.out.println("Polimorfismo: é o ato de classes aninhadas da mesma superclasse, herdarem o mesmo método o se comportarem de força distinta.");
 
 	}
 
